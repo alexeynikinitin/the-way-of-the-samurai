01e62ab4bbc744feb4b3daac1d16f65f277b7bfa
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-05 16:22:49 +0300

    Replased componet with a class "FindUsers"

--- src/components/Content/Users/FindUsers.js
+++ src/components/Content/Users/FindUsers.js
@@ -2,37 +2,45 @@ import classes from "./FindUsers.module.scss";
 import React from "react";
 import axios from "axios";
 
-let FindUsers = (props) => {
-  if (props.users.length === 0) {
+class FindUsers extends React.Component {
+  constructor(props) {
+    super(props);
     axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
       props.setUsers(response.data.items);
     })
   }
-  return (
-    <div>
-      {props.users.map(u =>
-        <div key={u.id} className={classes.user}>
-          <div className={classes.photo_btn}>
-            <div className={classes.photoBox}>
-              <img className={classes.photo} src={u.message} alt="photo"/>
+
+  render = () => {
+    return (
+      <div>
+        {this.props.users.map(u =>
+          <div key={u.id} className={classes.user}>
+            <div className={classes.photo_btn}>
+              <div className={classes.photoBox}>
+                <img className={classes.photo} src={u.message} alt="photo"/>
+              </div>
+              {
+                u.followed
+                  ? <button className={classes.btn} onClick={() => {
+                    this.props.followw(u.id)
+                  }}>follow</button>
+                  : <button className={classes.btn} onClick={() => {
+                    this.props.unfollow(u.id)
+                  }}>unfollow</button>
+              }
             </div>
-            {
-              u.followed
-                ? <button className={classes.btn} onClick={() => {props.followw(u.id)}}>follow</button>
-                : <button className={classes.btn} onClick={() => {props.unfollow(u.id)}}>unfollow</button>
-            }
-          </div>
-          <div className={classes.name_message}>
-            <p className={classes.name}>{u.name}</p>
-            <p className={classes.message}>u.message</p>
-          </div>
-          <div className={classes.country_city}>
-            <p className={classes.country}>u.location.country</p>
-            <p className={classes.city}>u.location.city</p>
-          </div>
-        </div>)}
-    </div>
-  )
+            <div className={classes.name_message}>
+              <p className={classes.name}>{u.name}</p>
+              <p className={classes.message}>u.message</p>
+            </div>
+            <div className={classes.country_city}>
+              <p className={classes.country}>u.location.country</p>
+              <p className={classes.city}>u.location.city</p>
+            </div>
+          </div>)}
+      </div>
+    )
+  }
 }
 
 export default FindUsers;
